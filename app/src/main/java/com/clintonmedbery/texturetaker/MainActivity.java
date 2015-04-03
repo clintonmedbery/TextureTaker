0e917758ccaea9a10a3bf49329ad89ea5a5f9ea3
clintomed <clintonmedbery@Clintons-Mac-Pro-3.local>
2015-04-03 01:53:44 -0400

    Updated picture to get a file path..
    Updated picture to get a file path and display in a square in a new
    activity.

--- app/src/main/java/com/clintonmedbery/texturetaker/MainActivity.java
+++ app/src/main/java/com/clintonmedbery/texturetaker/MainActivity.java
@@ -4,19 +4,27 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends ActionBarActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    final int PIC_CROP = 2;
+    static final int MEDIA_TYPE_IMAGE = 2;
+    public Uri imageUri;
 
-    private Uri picUri;
+
+    public Bitmap picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +57,8 @@ public class MainActivity extends ActionBarActivity {
 
     public void dispatchTakePictureIntent(View view){
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
+        imageUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
         if(takePictureIntent.resolveActivity(getPackageManager()) != null){
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
@@ -58,37 +67,49 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            picUri = data.getData();
 
-            performCrop();
+            Log.d("Debug", imageUri.getPath());
+
+            Intent editIntent = new Intent(this, PictureActivity.class);
+            editIntent.putExtra("ImagePath", imageUri.getPath());
+
+            startActivity(editIntent);
         }
-
-        if(requestCode == PIC_CROP && resultCode == RESULT_OK){
-            Bundle extras = data.getExtras();
-
-            Bitmap texturePic = extras.getParcelable("data");
-
-
-        }
-    }
-
-    private void performCrop(){
-        Intent cropIntent = new Intent("com.android.camera.action.CROP");
-
-        cropIntent.setDataAndType(picUri, "image/*");
-        cropIntent.putExtra("crop", "true");
-
-        cropIntent.putExtra("aspectX", 1);
-        cropIntent.putExtra("aspectY", 1);
-
-        cropIntent.putExtra("outputX", 256);
-        cropIntent.putExtra("outputY", 256);
-
-        cropIntent.putExtra("return-data", true);
-
-        startActivityForResult(cropIntent, PIC_CROP);
 
     }
 
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "TextureTaker");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("TextureTaker", "failed to create directory");
+                return null;
+            }
+        }
+        Log.d("TextureTaker", mediaStorageDir.toString());
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
 
 }
--- app/src/main/java/com/clintonmedbery/texturetaker/PictureActivity.java
+++ app/src/main/java/com/clintonmedbery/texturetaker/PictureActivity.java
@@ -1,17 +1,63 @@
 package com.clintonmedbery.texturetaker;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.ImageView;
 
 public class PictureActivity extends ActionBarActivity {
+
+    private Bitmap bitmap;
+    public ImageView imageView;
+    private Bitmap picture;
+    private String imagePath;
+
+    private int[] pixels;
+    private int imageHeight;
+    private int imageWidth;
+    private int screenWidth;
+    private int screenHeight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        Intent receiveIntent = getIntent();
+        imagePath = receiveIntent.getStringExtra("ImagePath");
+        picture = BitmapFactory.decodeFile(imagePath);
+
+        imageHeight = picture.getHeight();
+        imageWidth = picture.getWidth();
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        screenHeight = metrics.heightPixels;
+        screenWidth = metrics.widthPixels;
+
+        Log.d("Debug", "Image Height: " + String.valueOf(imageHeight));
+        Log.d("Debug", "Image Width: " + String.valueOf(imageWidth));
+        Log.d("Debug", "Screen Height: " + String.valueOf(screenHeight));
+        Log.d("Debug", "Screen Width: " + String.valueOf(screenWidth));
+
+
+        pixels = new int[imageHeight * imageWidth];
+        picture.getPixels(pixels, 0, imageWidth, 0, 30, imageWidth, imageWidth);
+        bitmap = Bitmap.createBitmap(pixels, imageWidth, imageWidth, Config.ARGB_8888);
+        imageView.getLayoutParams().height = screenWidth;
+        imageView.getLayoutParams().width = screenWidth;
+        imageView.setImageBitmap(bitmap);
+
     }
 
 
